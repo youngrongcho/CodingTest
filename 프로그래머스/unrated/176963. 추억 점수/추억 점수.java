0cffa629f1fd8030835b3406006d9d974b1440a3
youngrongcho <108881135+youngrongcho@users.noreply.github.com>
2023-04-21 17:35:23 +0900

    [unrated] Title: 추억 점수, Time: 0.07 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        for(int i=0; i<photo.length; i++){
+            int count = 0;
+            for(int j=0; j<photo[i].length; j++){
+                if(Arrays.asList(name).indexOf(photo[i][j])>=0) count+=yearning[Arrays.asList(name).indexOf(photo[i][j])];
+            }
+            answer[i] = count;
+        }
+        return answer;
+    }
+}
