28cc5c123477096418e3101458e1d47963d57e69
youngrongcho <108881135+youngrongcho@users.noreply.github.com>
2023-04-21 17:10:31 +0900

    [level 1] Title: 예산, Time: 0.33 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int answer = 0;
+        for(int i=0; i<d.length; i++){
+            budget -= d[i];
+            if(budget < 0) return answer;
+            answer++;
+        }
+        return answer;
+    }
+}
