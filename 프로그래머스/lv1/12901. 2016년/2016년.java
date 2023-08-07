1725ceae5514554833db0ab9b875c045ea1a8322
youngrongcho <108881135+youngrongcho@users.noreply.github.com>
2023-08-07 19:52:19 +0900

    [level 1] Title: 2016년, Time: 24.73 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.time.*;
+import java.time.format.TextStyle;
+
+class Solution {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase();
+    }
+}
